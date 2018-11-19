4f052545c457a880c7ebc6b757f102ed5812ea81
Merekeyev <T.Merekeyev@gmail.com>
2018-11-19 10:38:24 +0600

    genres

---
+++ MovieBestApp/View/GenresTableViewCell/GenresTableViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  GenresTableViewCell.swift
+//  MovieBestApp
+//
+//  Created by Temirlan Merekeyev on 11/19/18.
+//  Copyright © 2018 TemirlanMerekeyev. All rights reserved.
+//
+
+import UIKit
+
+class GenresTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var genreLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    
+    
+}
---
+++ MovieBestApp/View/ViewControllers/GenresViewController.swift
@@ -0,0 +1,21 @@
+//
+//  GenresViewController.swift
+//  MovieBestApp
+//
+//  Created by Temirlan Merekeyev on 11/19/18.
+//  Copyright © 2018 TemirlanMerekeyev. All rights reserved.
+//
+
+import UIKit
+
+class GenresViewController: UIViewController {
+
+    @IBOutlet weak var genresTableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+  
+}
